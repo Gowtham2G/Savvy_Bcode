cd61d03ca357d97694de7ce5f7ff8d070044eaf7
Gowtham2G <gowthamgadde2001@gmail.com>
2025-12-09 14:28:12 +0530

    User fixed

--- user/src/main/java/com/savvy/UserApplication.java
+++ user/src/main/java/com/savvy/UserApplication.java
@@ -2,9 +2,11 @@ package com.savvy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import com.savvy.config.SecurityConfig; // ✅ Import the class
+import org.springframework.context.annotation.Import; // ✅ Import this annotation
 
 @SpringBootApplication
+@Import(SecurityConfig.class)
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
